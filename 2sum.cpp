38d09fde47ff80847399af29393a92c75e8aa00f
FSD_ayush353 <a27392368@gmail.com>
2026-02-18 23:41:21 +0530

    first

---
+++ 2sum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int target=8;
+
+
+    for(int i=0;i<5;i++){
+
+     for(int j=i+1;j<5;j++){
+        if(arr[i]+arr[j]==target){
+            cout<<"yes";
+            cout<<i<<" "<<j;
+            return 0;
+        }
+     }
+
+    }
+    cout<<"no";
+    return 0;
+}
