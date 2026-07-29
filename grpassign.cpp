2b9da14bd85c98f41d00f1122aca347602f93cf2
FSD_ayush353 <a27392368@gmail.com>
2026-07-29 12:19:34 +0530

    assign

---
+++ grpassign.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n;
+int x;
+cout<<"enter the no of grps androll no ";
+cin>>n>>x;
+int t;
+while(t--){
+int p=2*n+1-x;
+cout<<p<<endl;
+}
+return 0;
+}
