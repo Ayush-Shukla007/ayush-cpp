d2487fb4091f11b202908beafbac72f37292c296
FSD_ayush353 <a27392368@gmail.com>
2026-07-29 12:32:59 +0530

    yoga

--- yoga.cpp
+++ yoga.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-
+int t;
+while(t){
 int n,x,y;
 cout<<"enter the total , earn from 1 and from 2";
 cin>>n>>x>>y;
+
 int maxx;
  int s1=n*x;
- int s2=n*y/2+(n%2)*x;
+ int s2=y*y/2+(n%2)*x;
  maxx=max(s1,s2);
  cout<<maxx;
-
-
+t--;
+}
 
 }
