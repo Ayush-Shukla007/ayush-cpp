e2b9a293d9eb4c419e8d11e5af7d59697f423281
FSD_ayush353 <a27392368@gmail.com>
2026-07-30 14:07:37 +0530

    bs

---
+++ binarys.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int>& nums, int target) {
+    int l = 0;
+    int r = nums.size() - 1;
+
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] < target)
+            l = mid + 1;
+        else
+            r = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> nums = {1, 3, 5, 7, 9, 11};
+    int target;
+
+    cout << "Enter target: ";
+    cin >> target;
+
+    int ans = binarySearch(nums, target);
+
+    if (ans != -1)
+        cout << "Found at index: " << ans << endl;
+    else
+        cout << "Not Found" << endl;
+
+    return 0;
+}
