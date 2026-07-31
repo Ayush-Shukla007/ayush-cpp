35ffeec56fd9db44038e9942665ad46476b33064
FSD_ayush353 <a27392368@gmail.com>
2026-07-31 15:17:35 +0530

    t2

---
+++ tree2.cpp
@@ -0,0 +1,24 @@
+ #include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+int main(){
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+
+    cout << "Root: " << root->val << endl;
+    cout << "Left Child: " << root->left->val << endl;
+    cout << "Right Child: " << root->right->val << endl;
+}
